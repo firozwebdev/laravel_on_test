4a59699b689981c9cf578c3d09d483ae986199c4
Sabuz <smskushtia@gmail.com>
2017-03-12 09:38:38 +0600

    first Commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use \Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+        //factory(App\User::class,10)->create();
+
+        factory(\App\User::class,50)->create()->each(function($u){
+            $u->posts()->save(factory(\App\Post::class)->make());
+        });
+    }
+}
